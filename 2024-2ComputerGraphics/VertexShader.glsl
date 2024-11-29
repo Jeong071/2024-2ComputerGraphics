a0d8aba3f76c0237b9aeda3c69c4ca77fda73ef7
winwin0620 <73763674+winwin0620@users.noreply.github.com>
2024-11-29 15:41:04 +0900

    셰이더 및 해상도
    해상도 1280x960, 버텍스/프래그먼트 셰이더 수정

--- 2024-2ComputerGraphics/VertexShader.glsl
+++ 2024-2ComputerGraphics/VertexShader.glsl
@@ -1,17 +1,18 @@
 #version 330 core
+layout (location = 0) in vec3 vPos;
+layout (location = 1) in vec3 vNormal;
 
-layout(location = 0) in vec3 position;
-layout(location = 1) in vec3 color;
+out vec3 FragPos; //--- 객체의 위치값을 프래그먼트 세이더로 보낸다.
+out vec3 Normal;  //--- 노멀값을 프래그먼트 세이더로 보낸다.
 
-out vec3 fragColor;
-
-uniform mat4 modelTransform;
-uniform mat4 viewTransform;
-uniform mat4 projectionTransform;
-
+uniform mat4 model;      //--- 모델링 변환값
+uniform mat4 view;       //--- 뷰잉 변환값
+uniform mat4 projection; //--- 투영 변환값
+uniform mat3 normalMatrix; //--- 법선 변환값
 
 void main()
 {
-    gl_Position = projectionTransform * viewTransform * modelTransform * vec4(position, 1.0);
-    fragColor = color;
+gl_Position = projection * view * model * vec4(vPos, 1.0);
+FragPos = vec3(model * vec4(vPos, 1.0)); // 월드 좌표계 위치
+Normal = normalize(normalMatrix * vNormal); // 정규화된 법선 벡터
 }
