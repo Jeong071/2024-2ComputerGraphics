1fc13fd3b184743117d03f7642ebbb89c65ce52c
Jeong071 <rldudwjd123@naver.com>
2024-12-01 17:16:04 +0900

    메인 Shader 제거
    메인 Shader 제거

--- 2024-2ComputerGraphics/main.cpp
+++ 2024-2ComputerGraphics/main.cpp
@@ -14,14 +14,8 @@ int main(int argc, char** argv) {
 	Initialize init(0,0,1280,960);
 	init.GLEWInit();
 
-	Shader sh;
-	vertexShader = sh.CreateVertexShader("VertexShader.glsl");		//VS 파일명
-	fragmentShader = sh.CreateFragmentShader("FragmentShader.glsl");	//FS 파일명
-	shaderProgramID = sh.CreateShaderProgram(vertexShader, fragmentShader);
-
 	gModel.ParseObj("Ghost.obj");
 	gModel.PrintInfo();
-	
 
 	Renderer draw;
 	glutDisplayFunc(draw.RenderScene);
